41afeaf56e5fbdd1a12875e9fe4e188c66855cd1
JG310712 <112426149+JG310712@users.noreply.github.com>
2023-01-16 15:31:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+function addUser() {
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCu80KdxsFdYYIEaFalsMm3Zyg_Sn56XO4",
-  authDomain: "kwitter-d72a9.firebaseapp.com",
-  projectId: "kwitter-d72a9",
-  storageBucket: "kwitter-d72a9.appspot.com",
-  messagingSenderId: "718621093248",
-  appId: "1:718621093248:web:97563409d4e6ed9f012565"
-};
+  user_name = document.getElementById("user_name").value;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  localStorage.setItem("user_name", user_name);
+  
+    window.location = "kwitter_room.html";
+}
+
